13c5e6a7d497f9496a64e79fe6576252bba1864d
Andy Firmansyah <andyfirmansyah2667@gmail.com>
2024-05-08 22:08:11 +0700

    feat: add new Collaboration section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Benefits from "./components/Benefits";
 // import Button from "./components/Button";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Collaboration from "./components/Collaboration";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
       </div>
       <ButtonGradient />
     </>
